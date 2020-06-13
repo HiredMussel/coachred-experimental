f1e57535ee6a8d74cf06274a1a759cac2be79a00
HiredMussel <alexmusselwhite2@gmail.com>
2020-06-13 17:19:21 +0100

    account for CORS

--- server/src/app/middleware.ts
+++ server/src/app/middleware.ts
@@ -5,4 +5,11 @@ import bodyParser = require('body-parser');
 export function middleware(app : express.Application) {
     app.use(bodyParser.json());
     app.use(expressMongoSanitize());
+    app.use(function(req : express.Request, res : express.Response, next : express.NextFunction) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Credentials', 'true')
+        next();
+    });
 }
